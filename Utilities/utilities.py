1f23b8a84a220029293531259ad492ee91acdb44
Paul Hoffman <hoff0792@umn.edu>
2016-09-02 17:52:37 -0500

    Expanded input options

--- Utilities/utilities.py
+++ Utilities/utilities.py
@@ -8,16 +8,16 @@ if sys.version_info.major is not 3:
 
 #   A function to remove duplicates from a list
 def deduplicate_list(with_dups, key):
-    """Deduplicate a list given a list, set, or tuple of values that should not be in the first list
+    """Deduplicate a list, set, or tuple given a list, set, or tuple of values that should not be in the first list
         with_dups is the list with values found in key"""
     try:
-        assert isinstance(with_dups, list)
+        assert isinstance(with_dups, list) or isinstance(with_dups, set) or isinstance(with_dups, tuple)
         assert isinstance(key, list) or isinstance(key, set) or isinstance(key, tuple)
     except AssertionError:
         raise
-    deduped = []
+    deduped = set()
     for value in with_dups:
-        if value not in key and value not in deduped:
-            deduped.append(value)
+        if value not in key:
+            deduped.add(value)
     print("Removed", len(with_dups) - len(deduped), "duplicates", file=sys.stderr)
     return deduped
